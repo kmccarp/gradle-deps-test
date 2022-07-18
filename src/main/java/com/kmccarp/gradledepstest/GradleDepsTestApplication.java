932e5f170b0010df0d6304ad2df896ab2b3b6293
Kevin McCarpenter™️ <kevin@moderne.io>
2022-07-18 16:57:22 -0500

    Initial commit

---
+++ src/main/java/com/kmccarp/gradledepstest/GradleDepsTestApplication.java
@@ -0,0 +1,13 @@
+package com.kmccarp.gradledepstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDepsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDepsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmccarp/gradledepstest/GradleDepsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmccarp.gradledepstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDepsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
